99924059fd52b8b8639894575b42f76454a12294
keukeu6 <s1f102401854@iniad.org>
2024-04-19 21:07:40 +0900

    Git hubに初保存

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to INIAD.ts</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
